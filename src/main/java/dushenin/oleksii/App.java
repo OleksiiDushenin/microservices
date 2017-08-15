60430c67e0ba99a6aafaf6a263f8b9d763ac45b1
Oleksii Dushenin <alexey.dushenin@gmail.com>
2017-08-15 19:49:37 +0300

    Initial commit

---
+++ src/main/java/dushenin/oleksii/App.java
@@ -0,0 +1,13 @@
+package dushenin.oleksii;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
