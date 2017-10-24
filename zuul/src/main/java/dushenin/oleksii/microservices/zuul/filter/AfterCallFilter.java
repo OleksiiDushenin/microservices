448a7f3a0dd0bb07458005ffe569839a71125ec5
Oleksii Dushenin <alexey.dushenin@gmail.com>
2017-10-24 11:19:18 +0200

    Papertrail added

--- zuul/src/main/java/dushenin/oleksii/microservices/zuul/filter/AfterCallFilter.java
+++ zuul/src/main/java/dushenin/oleksii/microservices/zuul/filter/AfterCallFilter.java
@@ -3,15 +3,18 @@ package dushenin.oleksii.microservices.zuul.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Component;
-
-import static dushenin.oleksii.microservices.zuul.filter.CorrelationIdUtils.CORRELATION_ID;
-import static dushenin.oleksii.microservices.zuul.filter.CorrelationIdUtils.getCorrelationId;
 
 @Slf4j
 @Component
 public class AfterCallFilter extends ZuulFilter {
+
+    public static final String CORRELATION_ID = "correlation-id";
+
+    @Autowired
+    private Tracer tracer;
 
     @Override
     public String filterType() {
@@ -30,17 +33,10 @@ public class AfterCallFilter extends ZuulFilter {
 
     @Override
     public Object run() {
-        try {
-            final String correlationId = getCorrelationId().orElse(null);
-            MDC.put(CORRELATION_ID, correlationId);
-            final RequestContext context = RequestContext.getCurrentContext();
-            context.getResponse().addHeader(CORRELATION_ID, correlationId);
-            log.info("Processing outcoming request for {}.", context.getRequest().getRequestURI());
-            return null;
-        } finally {
-            MDC.clear();
-        }
-
+        final RequestContext context = RequestContext.getCurrentContext();
+        context.getResponse().addHeader(CORRELATION_ID, tracer.getCurrentSpan().traceIdString());
+        log.info("Processing outcoming request for {}.", context.getRequest().getRequestURI());
+        return null;
     }
 
 }
