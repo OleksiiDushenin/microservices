beedee30bbabca3c322dc10cdb79507566dd4ddc
Oleksii Dushenin <alexey.dushenin@gmail.com>
2017-11-19 16:14:53 +0200

    fix travis docker

--- travis/push_to_docker_hub.sh
+++ travis/push_to_docker_hub.sh
@@ -3,9 +3,16 @@
 echo "Pushing images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker push dushenin.oleksii.microservices.zipkin::$BUILD_NAME
-docker push dushenin.oleksii.microservices.zuul:$BUILD_NAME
-docker push dushenin.oleksii.microservices.discovery:$BUILD_NAME
-docker push dushenin.oleksii.microservices.config:$BUILD_NAME
-docker push dushenin.oleksii.microservices.recommendations:$BUILD_NAME
-docker push dushenin.oleksii.microservices.movies:$BUILD_NAME
+docker tag dushenin.oleksii.microservices.zipkin:$BUILD_NAME alexeydushenin/dushenin.oleksii.microservices.zipkin:$BUILD_NAME
+docker tag dushenin.oleksii.microservices.zuul:$BUILD_NAME alexeydushenin/dushenin.oleksii.microservices.zuul:$BUILD_NAME
+docker tag dushenin.oleksii.microservices.discovery:$BUILD_NAME alexeydushenin/dushenin.oleksii.microservices.discovery:$BUILD_NAME
+docker tag dushenin.oleksii.microservices.config:$BUILD_NAME alexeydushenin/dushenin.oleksii.microservices.config:$BUILD_NAME
+docker tag dushenin.oleksii.microservices.recommendations:$BUILD_NAME alexeydushenin/dushenin.oleksii.microservices.recommendations:$BUILD_NAME
+docker tag dushenin.oleksii.microservices.movies:$BUILD_NAME alexeydushenin/dushenin.oleksii.microservices.movies:$BUILD_NAME
+
+docker push alexeydushenin/dushenin.oleksii.microservices.zipkin:$BUILD_NAME
+docker push alexeydushenin/dushenin.oleksii.microservices.zuul:$BUILD_NAME
+docker push alexeydushenin/dushenin.oleksii.microservices.discovery:$BUILD_NAME
+docker push alexeydushenin/dushenin.oleksii.microservices.config:$BUILD_NAME
+docker push alexeydushenin/dushenin.oleksii.microservices.recommendations:$BUILD_NAME
+docker push alexeydushenin/dushenin.oleksii.microservices.movies:$BUILD_NAME
